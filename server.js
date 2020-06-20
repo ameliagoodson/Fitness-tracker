f8e1221d897f5b4c4283156442a5dce48461091f
Amelia Goodson <ameliajanegoodson@gmail.com>
2020-06-21 09:10:33 +1000

    amendments

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require ("path")
 
 const PORT = process.env.PORT || 3000;
 
-const Exercise = require("./models/workout.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -18,23 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", { useNewUrlParser: true });
 
-router.post("/api/workouts", (req, res) => {
-console.log('Hello world')
-  Exercise.create({
-    
-  })
-    .then(Workout => {
-      res.json(Workout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
-router.get("/exercise", (req, res) => {
-  console.log('Hello world')
-    res.sendFile(path.join(__dirname + "public/exercise.html"))
-  });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
